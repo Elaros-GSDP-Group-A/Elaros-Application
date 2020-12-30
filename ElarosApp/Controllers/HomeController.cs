b5ef9646cc8fcf6b73897a426ae8567e397860f6
= <me@alta.sh>
2020-12-30 21:16:25 +0000

    Created basic in-memory referal code validation

--- ElarosApp/Controllers/HomeController.cs
+++ ElarosApp/Controllers/HomeController.cs
@@ -11,9 +11,30 @@ namespace ElarosApp.Controllers
 {
     public class HomeController : Controller
     {
+        // In memory temporary patient code list 
+        private List<string> patientCodeList = new List<string>() { "unggz", "QPbs8", "Tl0vW", "s0lIx", "yXo79" };
+        private string _referalCode;
+        [HttpPost]
+        public ActionResult Index(string referalCode)
+        {
+            _referalCode = referalCode;
+            if (patientCodeList.Contains(referalCode))
+            {
+                return Content($"Successfully logged in as {referalCode}");
+            }
+            return Index();
+        }
+
+        [HttpGet]
         public ActionResult Index()
         {
+            if (ModelState.IsValid)
+            {
+                return View("StartQuestionnaire", _referalCode);
+            }
             return View("StartQuestionnaire");
         }
+
+        
     }
 }
