352da87e590fefcc2805e9627a97210ab70a43fe
= <me@alta.sh>
2021-01-15 15:46:47 +0000

    Clinician portal development

---
+++ ElarosApp/Controllers/PortalController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ElarosApp.Models.Clinician;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElarosApp.Controllers
+{
+    public class PortalController : Controller
+    {
+        public IActionResult Index(LoginFormModel Clinician)
+        {
+            return View("../Clinician/Portal", Clinician);
+        }
+    }
+}
