99e29c8251940509a03a070734db9a2862a8425d
CrazyOldBuffalo <tomasz.boberek@gmail.com>
2021-02-16 11:10:09 +0000

    Added Post to Clinician For Creating new Patients.

--- ElarosApp/Controllers/PortalController.cs
+++ ElarosApp/Controllers/PortalController.cs
@@ -4,14 +4,30 @@ using System.Linq;
 using System.Threading.Tasks;
 using ElarosApp.Models.Clinician;
 using Microsoft.AspNetCore.Mvc;
+using ElarosApp.Data;
 
 namespace ElarosApp.Controllers
 {
     public class PortalController : Controller
     {
+
+        private readonly DataContext _context;
+
+        public PortalController(DataContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index(LoginFormModel Clinician)
         {
             return View("../Clinician/Portal", Clinician);
+        }
+
+        [HttpPost]
+        public IActionResult Index(string TextBoxRefCode)
+        {
+            _context.Patients.Add(new Models.PatientModel { ReferalCode = TextBoxRefCode, FinishedQuestionniare = false, QuestionId = 1 });
+            _context.SaveChanges();
+            return View("../Clinician/Portal");
         }
     }
 }
